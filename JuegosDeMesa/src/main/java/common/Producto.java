0a99b115d5e1a22038f83930a29c620d917d38a8
Ignacio G <Ignacio.espacio@gmail.com>
2013-02-26 12:23:13 +0100

    cambios (Producto)

--- JuegosDeMesa/src/main/java/common/Producto.java
+++ JuegosDeMesa/src/main/java/common/Producto.java
@@ -6,5 +6,9 @@ package common;
  * @author Ignacio González Granados
  */
 public class Producto {
-    
+    private int id;
+    private String nombre_producto;
+    private String descripcion_producto;
+    private String enlace_producto;
+    private int id_categoria;
 }
