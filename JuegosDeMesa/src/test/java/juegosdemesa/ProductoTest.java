d3d67d2121dc4e368acf84e6a586063f1b84a753
Ignacio G <Ignacio.espacio@gmail.com>
2013-02-26 13:50:40 +0100

    ProductoTest(EnBlanco)

--- JuegosDeMesa/src/test/java/juegosdemesa/ProductoTest.java
+++ JuegosDeMesa/src/test/java/juegosdemesa/ProductoTest.java
@@ -2,8 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package juegosdemesa;
 
+import common.Categoria;
+import common.Producto;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -20,7 +23,7 @@ import org.junit.Test;
  *
  * @author Usuario
  */
-
+/*
 public class ProductoTest {
     
     private EntityManager em;
@@ -56,26 +59,53 @@ public class ProductoTest {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("cursoDemoPU");
         em = emf.createEntityManager();
 
-        //
-        /*
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        */
-        /*
-        ago = new Author(1, "George Orwell", sdf.parse("1903-06-25"), "United Kingdom");
-        ajkr = new Author(2, "J. K. Rowling", sdf.parse("1965-07-31"), "United Kingdom");
-        ame = new Author(3, "Michael Ende", sdf.parse("1929-11-12"), "Germany");
-        ada = new Author(4, "Douglas Adams", sdf.parse("1952-03-11"), "United Kingdom");
-
-        b1984 = new Book("978 0 140 81774 4", "Nineteen eighty-four", 1949);
-        b1984.setAuthor(ago);
-        bhp1 = new Book("0 7475 5819 1", "Harry Potter and the philosopher's stone", 1997);
-        bhp1.setAuthor(ajkr);
-        bhp2 = new Book("0 7475 3848 4", "Harry Potter and the chamber of secrets", 1998);
-        bhp2.setAuthor(ajkr);
-        bhi = new Book("35 221 2800 1", "Die unendliche geschichte", 1979);
-        bhi.setAuthor(ame);
-        bga = new Book("0 345 39180 2", "The hitchhicker's guide to the galaxy", 1979);
-        bga.setAuthor(ada);
-        * */
+        categoria1=new Categoria(1,"Juegos para niños","Juego simple");
+        categoria2=new Categoria(2,"Juegos para adolescentes","Juego menos simple");
+        categoria3=new Categoria(3,"Juegos para adultos","Juego complejo");
+        categoria4=new Categoria(4,"Juegos para ancianos","Juego muy complejo");
+        
+        producto1=new Producto("Cartago contra Roma","Cartago lucha contra Roma","http://www.planetongames.com/sword-rome-2010-reprint-p-2403.html");
+        producto1.setCategoria(categoria1);
+        producto2=new Producto("The Havre","Juego de barcos y comida","http://www.planetongames.com/le-havre-p-1572.html");
+        producto1.setCategoria(categoria1);
+        producto3=new Producto("7 ages","Juego de fichitas","http://www.planetongames.com/7-wonders-p-2430.html");
+        producto1.setCategoria(categoria2);
+        producto4=new Producto("Agricola","Juego de gestión","http://www.planetongames.com/agricola-castellano-p-732.html");
+        producto1.setCategoria(categoria3);
+        producto5=new Producto("Here I Stand","Juego belico","http://www.planetongames.com/here-stand-2010-reprint-p-329.html");
+        producto1.setCategoria(categoria4);
+        
     }
+    @Test
+    @Ignore
+    public void test_addProducto_ProductoNotExists() {
+        boolean result = producto1.insertarProducto(em);
+        result = result && producto1.insertarProducto(em);
+        assertTrue(result);
+        //
+        assertTrue(Producto.containsBook(em, Producto.getId()));
+        assertEquals(1, Book.count(em));
+
+        assertEquals(bhp1, Book.findById(em, bhp1.getId()));
+    }
+    /*
+    @Test
+    @Ignore
+    public void test_addProducto_ProductoExists() {
+        ajkr.create(em);
+        bhp1.create(em);
+
+        boolean result = bhp1.create(em);
+        assertFalse(result);
+
+        Book book1 = bhp1.clone();
+        book1.setId(0);
+        result = book1.create(em);
+        assertFalse(result);
+
+        assertEquals(1, Book.count(em));
+    }
+    
+    
 }
+*/
